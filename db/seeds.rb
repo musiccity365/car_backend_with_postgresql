12f295be240ded66e6bc4f74c52659cd3226c723
musiccity365 <m_sackstein@yahoo.com>
2021-07-27 21:36:45 -0400

    adjust migrations and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Origin.destroy_all
+Car.destroy_all
+
+domestic = Origin.create(name: 'Domestic')
+import = Origin.create(name: 'Import')
+
+Car.create([
+  {year: '2014', make: 'Honda', model: 'CR-V', origin: import},
+  {year: '2003', make: 'Honda', model: 'Civic', origin: import},
+  {year: '2007', make: 'Chevy', model: 'Suburban', origin: domestic},
+  {year: '1986', make: 'Ford', model: 'Mustang', origin: domestic},
+  {year: '2000', make: 'Audi', model: 'TT', origin: import},
+  {year: '2000', make: 'VW', model: 'Passat', origin: import},
+  {year: '2005', make: 'Jeep', model: 'Laredo', origin: domestic}
+])
