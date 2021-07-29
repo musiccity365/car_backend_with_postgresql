f6b3e8443a3969ee712f2d7d1e41f377afef97a2
musiccity365 <m_sackstein@yahoo.com>
2021-07-28 22:03:01 -0400

    idk

--- db/migrate/20210728004257_create_cars.rb
+++ db/migrate/20210728004257_create_cars.rb
@@ -1,11 +1,11 @@
 class CreateCars < ActiveRecord::Migration[6.1]
   def change
     create_table :cars do |t|
+      t.integer :year
       t.string :make
       t.string :model
       t.string :image_url
-      t.integer :origin_id
-
+      
       t.timestamps
     end
   end
