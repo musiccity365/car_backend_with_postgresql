5ddf3f2e6ade8bb504791652e5711ebc7eee099b
musiccity365 <m_sackstein@yahoo.com>
2021-07-28 22:35:06 -0400

    drafting resource namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # use namespace in our backend to indicate that our routes are associated with the api
+  namespace :api do
+    namespace :v1 do # version 1
+      resources :cars, only: [:index]
+    end
+  end
 end
